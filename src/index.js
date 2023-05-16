7fa67c06d8eb10d7df0164c1d84ba966c5269983
ItsZeroFour <ItsZeroFourX@gmail.com>
2023-05-16 17:38:25 +0300

    change base name

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/weatherApp">
+  <BrowserRouter basename="/weatherapp">
     <React.StrictMode>
       <App />
     </React.StrictMode>
